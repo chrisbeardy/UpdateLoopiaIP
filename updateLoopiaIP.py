24fe44710a3127bcb0099425b0ec38f2d87d74d8
chrisbeardy <20585410+chrisbeardy@users.noreply.github.com>
2021-05-10 09:14:36 +0100

    ready for test

--- updateLoopiaIP.py
+++ updateLoopiaIP.py
@@ -3,13 +3,13 @@
 
 import logging
 from logging.handlers import RotatingFileHandler
-import configparser
+import json
 import requests
 from sys import exit
 import xmlrpc.client
 
 
-def updateLoopiaIP(domain, uname, passw):
+def updateLoopiaIP(domains, subdomains, username, password):
     logger.debug('Updating IP')
     try:
         ip = requests.get('https://ifconfig.me') # could use https://icanhazip.com, http://checkip.dyndns.org
@@ -20,27 +20,21 @@ def updateLoopiaIP(domain, uname, passw):
         logger.critical(e)
         exit()
 
+    domain_server_url = 'https://api.loopia.se/RPCSERV' 
     try:
-        username = uname
-        password = passw
-        domain_server_url = 'https://api.loopia.se/RPCSERV' 
-
-        domain = domain
-        subdomain = '@'
-
         client = xmlrpc.client.ServerProxy(domain_server_url, encoding = 'utf-8')
+        for d, sd in zip(domains, subdomains):
+            old_record_obj = client.getZoneRecords(username, password, d, sd)
 
-        old_record_obj = client.getZoneRecords(username, password, domain, subdomain)
+            record_id = old_record_obj[0]['record_id'] 
+            old_ip = old_record_obj[0]['rdata']
 
-        record_id = old_record_obj[0]['record_id'] 
-        old_ip = old_record_obj[0]['rdata']
-
-        if old_ip == ip:
-            logger.debug('IP already matches')
-        else:
-            record_obj = {'type': 'A', 'ttl': 300, 'priority': 0, 'rdata': ip, 'record_id': record_id}
-            response = client.updateZoneRecord(username, password, domain, subdomain, record_obj)
-            logger.debug('IP update Loopia response: '+ response)
+            if old_ip == ip:
+                logger.debug('IP already matches')
+            else:
+                record_obj = {'type': 'A', 'ttl': 300, 'priority': 0, 'rdata': ip, 'record_id': record_id}
+                response = client.updateZoneRecord(username, password, d, sd, record_obj)
+                logger.debug('IP update Loopia response: '+ response)
     except Exception as e:
         logger.critical('Loopia IP update failed')
         logger.critical(e)
@@ -53,19 +47,28 @@ if __name__ == '__main__':
     log_formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
     log_handler.setFormatter(log_formatter)
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     logger.addHandler(log_handler)
 
     # config
-    config = configparser.ConfigParser()
-    config.read('updateLoopiaIPConfig.cfg')
     try:
-        domain = config['credentials']['domain']
-        uname = config['credentials']['uname']
-        passw = config['credentials']['passw']
+        with open('updateLoopiaIPConfig.json') as file:
+            config = json.load(file)
+    except (IOError, FileNotFoundError, json.JSONDecodeError):
+        logger.critical('Config file not OK')
+        exit()
+    try:
+        domains = config['domains']
+        subdomains = config['subdomains']
+        username = config['username']
+        password = config['password']
     except Exception as e:
         logger.critical('Config file inaccurate')
         exit()
+        
+    if type(domains) is not list:
+        domains = [domains]
+    if type(subdomains) is not list:
+        subdomains = [subdomains]
 
-    updateLoopiaIP(domain, uname, passw)  
-
+    updateLoopiaIP(domains, subdomains, username, password)  
